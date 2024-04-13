f076e61fa364cc42499cb1695508a39cabde2993
itsDru157 <carlosmateus7720@gmail.com>
2024-04-13 06:38:00 -0300

    codigo revisado em Breves atualizacoes

---
+++ main.py
@@ -0,0 +1,21 @@
+#GERADOR DE SENHA
+import string 
+import random as r
+from itertools import chain
+NUMERO_DE_REPETICOES = 12
+tamanho_senha = 0
+letras_minusculas = string.ascii_lowercase
+numeros = "0123456789"
+simbolos = "!@#$%^&*()-_+={}[]|;':<>,.?/"
+def gerar_senha(tamanho_senha):
+    todas_letras = list(letras_minusculas + numeros + simbolos)  # Converte em lista
+    r.shuffle(todas_letras)
+    senha = "".join(map(str, r.sample(todas_letras, tamanho_senha)))
+    print(senha)
+print("Sua senha é: ")
+gerar_senha(12)  # Chamando a função com o tamanho da senha desejado
+    
+        
+        
+        
+     
